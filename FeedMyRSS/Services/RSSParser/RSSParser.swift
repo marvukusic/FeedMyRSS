dd5f7f2d65a11debea347d8df1c3e756d9eee47f
marvukusic <marvukusic@gmail.com>
2024-11-05 19:59:36 +0100

    Implement XMLParser delegate functions

--- FeedMyRSS/Services/RSSParser/RSSParser.swift
+++ FeedMyRSS/Services/RSSParser/RSSParser.swift
@@ -37,20 +37,74 @@ class RSSParser: NSObject {
 
 extension RSSParser: XMLParserDelegate {
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        currentText = ""
+        
+        switch RssKeys(rawValue: elementName) {
+        case .item:
+            currentItem = RSSItem()
+            
+        case .media:
+            guard currentItem?.imageURL == nil else { break }
+            currentItem?.imageURL = URL(string: attributeDict[RssAttributes.url.rawValue] ?? "")
+            
+        default:
+            break
+        }
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
+        guard !trimmed(string).isEmpty else { return }
+        currentText += trimmed(string)
     }
     
     func parser(_ parser: XMLParser, foundCDATA CDATABlock: Data) {
+        currentText += trimmed(String(data: CDATABlock, encoding: .utf8) ?? "")
     }
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        switch RssKeys(rawValue: elementName) {
+        case .title:
+            guard currentItem != nil else {
+                feed.title = currentText
+                return
+            }
+            currentItem?.title = currentText
+            
+        case .description:
+            guard currentItem != nil else {
+                feed.description = currentText
+                return
+            }
+            currentItem?.description = currentText
+            
+        case .url:
+            feed.imageURL = URL(string: currentText)
+            
+        case .link:
+            currentItem?.linkURL = URL(string: currentText)
+            
+        case .item:
+            if let item = currentItem {
+                feed.items.append(item)
+            }
+            currentItem = nil
+            
+        default:
+            break
+        }
+        
     }
     
     func parserDidEndDocument(_ parser: XMLParser) {
+        completion?(.success(feed))
     }
     
     func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        completion?(.failure(.errorParsingXML))
+    }
+    
+    private func trimmed(_ string: String) -> String {
+        return string.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
