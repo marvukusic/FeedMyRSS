0328d27f896e4f8bc6ddb86c237c0e0f2972ef91
marvukusic <marvukusic@gmail.com>
2024-11-05 19:23:27 +0100

    Initial Commit

---
+++ FeedMyRSS/FeedMyRSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedMyRSSApp.swift
+//  FeedMyRSS
+//
+//  Created by Marko Vukušić on 05.11.2024..
+//
+
+import SwiftUI
+
+@main
+struct FeedMyRSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FeedMyRSSTests/FeedMyRSSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeedMyRSSTests.swift
+//  FeedMyRSSTests
+//
+//  Created by Marko Vukušić on 05.11.2024..
+//
+
+import Testing
+@testable import FeedMyRSS
+
+struct FeedMyRSSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
