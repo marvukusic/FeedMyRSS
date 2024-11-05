0ce03b9f9fd50794b6184286fb7fa22dc5abc479
marvukusic <marvukusic@gmail.com>
2024-11-05 19:29:38 +0100

    Create RSSItem model

---
+++ FeedMyRSS/Services/RSSParser/Models/RSSItem.swift
@@ -0,0 +1,15 @@
+//
+//  RSSItem.swift
+//  FeedMyRSS
+//
+//  Created by Marko Vukušić on 05.11.2024.
+//
+
+import Foundation
+
+struct RSSItem {
+    var title: String = ""
+    var description: String = ""
+    var imageURL: URL?
+    var linkURL: URL?
+}
