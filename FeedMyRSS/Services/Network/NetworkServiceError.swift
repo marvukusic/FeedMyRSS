aa11498b6d98d0e8fceec1a43f43ee3c52105928
marvukusic <marvukusic@gmail.com>
2024-11-06 10:57:04 +0100

    Create NetworkServiceError enum

---
+++ FeedMyRSS/Services/Network/NetworkServiceError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkServiceError.swift
+//  FeedMyRSS
+//
+//  Created by Marko Vukušić on 06.11.2024..
+//
+
+import Foundation
+
+enum NetworkServiceError: Error {
+    case invalidURL
+}
