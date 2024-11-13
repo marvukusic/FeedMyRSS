ea2bc09c11e17564a9531ac17931c0e6127d4608
marvukusic <marvukusic@gmail.com>
2024-11-13 16:31:53 +0100

    Extend strings with pluraliseIfNeeded method

--- FeedMyRSS/Extensions/String+Extensions.swift
+++ FeedMyRSS/Extensions/String+Extensions.swift
@@ -12,4 +12,9 @@ extension String {
         guard let components = URLComponents(string: self) else { return false }
         return ["http", "https"].contains(components.scheme?.lowercased()) && components.host != nil
     }
+    
+    func pluraliseIfNeeded(for count: Int) -> String {
+        let locval: String.LocalizationValue = "^[\(count) \(self)](inflect: true)"
+        return String(AttributedString(localized: locval).characters)
+    }
 }
