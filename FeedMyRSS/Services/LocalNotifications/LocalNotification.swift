57b505435d2554c277d1178b8e6f563fe70da743
marvukusic <marvukusic@gmail.com>
2024-11-13 13:17:11 +0100

    Choose right notification delegate to receive local notification

--- FeedMyRSS/Services/LocalNotifications/LocalNotification.swift
+++ FeedMyRSS/Services/LocalNotifications/LocalNotification.swift
@@ -9,12 +9,10 @@ import Foundation
 import NotificationCenter
 
 struct LocalNotification {
-    let id: String
     let title: String
     let subtitle: String
     
-    init(id: String = UUID().uuidString, title: String, subtitle: String = "") {
-        self.id = id
+    init(title: String, subtitle: String = "") {
         self.title = title
         self.subtitle = subtitle
     }
@@ -23,8 +21,9 @@ struct LocalNotification {
         let content = UNMutableNotificationContent()
         content.title = title
         content.body = subtitle
+        content.sound = .default
         
-        let request = UNNotificationRequest(identifier: id, content: content, trigger: nil)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
         UNUserNotificationCenter.current().add(request) { _ in }
     }
 }
