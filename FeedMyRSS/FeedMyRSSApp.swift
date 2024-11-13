0cbb86406a38991dfd281c222c7b25427675c87e
marvukusic <marvukusic@gmail.com>
2024-11-13 12:35:49 +0100

    Implement notification permission request on startup

--- FeedMyRSS/FeedMyRSSApp.swift
+++ FeedMyRSS/FeedMyRSSApp.swift
@@ -9,10 +9,31 @@ import SwiftUI
 
 @main
 struct FeedMyRSSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        requestNotificationPermission()
+        return true
+    }
+    
+    private func requestNotificationPermission() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound]) { _, _ in }
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("Notification received with identifier: \(response.notification.request.identifier)")
+        completionHandler()
     }
 }
