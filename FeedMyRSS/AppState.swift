628730e1db37e003fec29ae822f1f3461e8d4ff1
marvukusic <marvukusic@gmail.com>
2024-11-13 16:34:01 +0100

    Create AppState singleton

---
+++ FeedMyRSS/AppState.swift
@@ -0,0 +1,16 @@
+//
+//  AppState.swift
+//  FeedMyRSS
+//
+//  Created by Marko Vukušić on 13.11.2024.
+//
+
+import Combine
+
+class AppState: ObservableObject {
+    static let shared = AppState()
+    
+    @Published var shouldRefreshFeed = false
+    
+    private init() {}
+}
