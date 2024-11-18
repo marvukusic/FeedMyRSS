ffc3516b87b12a8f34d27b6204f18f6339e62a13
marvukusic <marvukusic@gmail.com>
2024-11-18 14:02:41 +0100

    Improve UI test as doubleTap to delete text was not reliable

--- FeedMyRSSUITests/FeedMyRSSUITests.swift
+++ FeedMyRSSUITests/FeedMyRSSUITests.swift
@@ -31,24 +31,35 @@ final class FeedMyRSSUITests: XCTestCase {
     func testAddAndDeleteNewRSSFeed() {
         let feedURL = "https://feeds.bbci.co.uk/news/world/rss.xml"
         
+        /// Clear existing cells, if any
         let cells = app.collectionViews["feedList"].cells
         if cells.count > 0 {
             clearFeedList(cells: cells)
         }
         
+        /// Open add new feed popup
         app.buttons["Add New Feed"].tap()
         XCTAssertTrue(app.alerts["Add new RSS feed"].exists, "Add new RSS feed alert did not appear")
         
+        /// Delete any text in input field, if any
         let urlTextField = app.textFields["URL"]
         XCTAssertTrue(urlTextField.exists, "URL text field did not appear")
-
-        urlTextField.doubleTap()
+        if let fieldValue = urlTextField.value as? String {
+            let deleteString = fieldValue.map({ _ in "\u{8}" }).joined()
+            urlTextField.typeText(deleteString)
+        }
+        
+        /// Write test URL in input field
         urlTextField.typeText(feedURL)
+        
+        /// Accept inserted URL
         app.alerts["Add new RSS feed"].buttons["OK"].tap()
         
+        /// Check if feed was added to the list
         let addedCell = cells.element(boundBy: 0)
         XCTAssertTrue(addedCell.waitForExistence(timeout: 1))
         
+        /// Clear feed list
         clearFeedList(cells: cells)
     }
     
